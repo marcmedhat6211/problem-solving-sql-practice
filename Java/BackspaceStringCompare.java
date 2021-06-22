8304e31f47f694434380e43ade4065de50a59475
Marc Medhat <marcmedhat6211@gmail.com>
2021-06-22 07:25:55 +0200

    new problems

---
+++ Java/BackspaceStringCompare.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character> sStack = new Stack<Character>();
+        for(char c: s.toCharArray()) {
+            if(c != '#') {
+                sStack.push(c);
+            } else if(!sStack.isEmpty()) {
+                sStack.pop();
+            }
+        }
+        
+        Stack<Character> tStack = new Stack<Character>();
+        for(char c: t.toCharArray()) {
+            if(c != '#') {
+                tStack.push(c);
+            } else if(!tStack.isEmpty()) {
+                tStack.pop();
+            }
+        }
+        
+        while(!sStack.isEmpty()) {
+            char current = sStack.pop();
+            if(tStack.isEmpty() || tStack.pop() != current) {
+                return false;
+            }
+        }
+        
+        return sStack.isEmpty() && tStack.isEmpty();
+    }
+}
